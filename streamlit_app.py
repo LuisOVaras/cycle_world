e206cd2b037e92929eec1a7898aad89973939811
Luis Varas <96844985+LuisOVaras@users.noreply.github.com>
2025-04-16 15:04:05 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,15 +24,25 @@ df_2_llegada = session.table("CYCLE_WORLD.PUBLIC.REPORT_2_LLEGADA")
 df_2_salida = session.table("CYCLE_WORLD.PUBLIC.REPORT_2_SALIDA")
 #st.dataframe(data=df_2_salida, use_container_width=True)
 
-
 # 
 df_3_llegada_marylebone = session.table("CYCLE_WORLD.PUBLIC.REPORT_2_LLEGADA").filter(col("sector") == ' Marylebone')
 #st.dataframe(data=df_3_llegada_marylebone, use_container_width=True)
 
 df_3_salida_marylebone = session.table("CYCLE_WORLD.PUBLIC.REPORT_2_SALIDA").filter(col("sector") == ' Marylebone')
 #st.dataframe(data=df_3_salida_marylebone, use_container_width=True)
+
+# Convertir a pandas DataFrame
+df1 = df_1.to_pandas()
+df2_llegada = df_2_llegada.to_pandas()
+df2_salida = df_2_salida.to_pandas()
+df3_llegada_marylebone = df_3_llegada_marylebone.to_pandas()
+df3_salida_marylebone = df_3_salida_marylebone.to_pandas()
+
+
+
+
 fig, ax = plt.subplots(figsize=(10, 5))
-sns.barplot(data=df_1, x="estacion", y="viajes", ax=ax)
+sns.barplot(data=df1, x="estacion", y="viajes", ax=ax)
 ax.set_title("Cantidad de viajes por estación")
 ax.set_xticklabels(ax.get_xticklabels(), rotation=45, ha="right")
 st.pyplot(fig)
